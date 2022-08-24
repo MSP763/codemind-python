ea36256ed973239a7063d8ea69eae4854a0eab8a
21A91A0583@aec.edu.in <97598280+MSP763@users.noreply.github.com>
2022-08-24 14:57:55 +0530

    x=input().split() ch='aeuioAEUIO' c=0 d=[] for i in x:     j=str(i)     c=0     for k in j:         if k in ch:             c+=1     d.append(c) g=min(d) print(d.count(g))

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,13 @@
+x=input().split()
+ch='aeuioAEUIO'
+c=0
+d=[]
+for i in x:
+    j=str(i)
+    c=0
+    for k in j:
+        if k in ch:
+            c+=1
+    d.append(c)
+g=min(d)
+print(d.count(g))
