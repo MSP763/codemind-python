7015e6a56107ff12ed441065fd8f04946b856ae3
21A91A0583@aec.edu.in <97598280+MSP763@users.noreply.github.com>
2022-09-06 06:57:01 +0530

    def pir(i):     for j in range(2,int(i**0.5)+1):         if i%j==0:             return 0     return 1 a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if(pir(i) and i!=1):         c+=1 print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,12 +1,12 @@
-def prime(n):
-    for i in range(2,int(n**0.5)+1):
-        if(n%i==0):
+def pir(i):
+    for j in range(2,int(i**0.5)+1):
+        if i%j==0:
             return 0
     return 1
-x=int(input())
-y=int(input())
+a=int(input())
+b=int(input())
 c=0
-for i in range(x,y+1):
-    if prime(i) and i!=1 :
+for i in range(a,b+1):
+    if(pir(i) and i!=1):
         c+=1
 print(c)
